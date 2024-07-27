724b7724ca8a9a4c560734a0285d0273dd21393a
prashant <prashanth@shikshalokam.org>
2024-07-27 15:37:02 +0530

    replaced all values with null

--- dummy_data_ingestion/obs_sur_dummy_data_ingest.py
+++ dummy_data_ingestion/obs_sur_dummy_data_ingest.py
@@ -14,23 +14,79 @@ kafka_url = config.get("KAFKA", "url")
 producer = KafkaProducer(bootstrap_servers=[kafka_url])
 
 # Dummy data
-slObservation = {'roleTitle': '', 'userBoardName': None, 'userType': '', 'organisationId': '01392275379456409617', 'organisationName': '', 'observationSubmissionId': '60111ac82d0bbd2f0c3229ce', 'appName': '', 'solutionType': 'observation_with_out_rubric', 'entity': 'do_1234567', 'entityExternalId': '', 'course': 'do_1234567', 'createdBy': '828a16a4-a20f-4ed5-b8f5-7a96770a31c1', 'isAPrivateProgram': False, 'programExternalId': 'Testing Observation for Support', 'programId': '660150444692d300086005c6', 'programName': 'Testing Observation for Support', 'programDescription': 'Program', 'solutionExternalId': '82fb3fe4-ea91-11ee-9c5f-d5a85c1801d4-OBSERVATION-TEMPLATE_CHILD', 'solutionId': '6601504bd26a000008496966', 'observationId': '660253ec076ebd0008b14fdd', 'criteriaExternalId': 'C1_1711362113845-1711362123655', 'criteriaName': 'Criteria 1', 'criteriaDescription': 'Criteria 1', 'section': '', 'solutionName': 'Testing Observation for Support', 'scoringSystem': 'None', 'solutionDescription': 'Observation Solution', 'questionSequenceByEcm': None, 'entityType': 'course', 'observationName': 'Testing Observation for Support', 'questionId': '65fd59e9d2bc56000784c467', 'questionAnswer': 'gag', 'questionResponseType': 'text', 'questionResponseLabel': "'gag'", 'questionExternalId': 'SUR_TEST_001_1710387379109-1710387389092', 'questionName': 'What are the challenges that you are facing in enrolment?', 'questionECM': 'OB', 'criteriaId': '65fd59e9d2bc56000784c478', 'completedDate': '2024-03-26T16:14:10.690Z', 'createdAt': '2024-03-26T16:12:49.079Z', 'updatedAt': '2024-03-26T16:14:10.691Z', 'remarks': '', 'totalEvidences': 0, 'instanceParentQuestion': '', 'instanceParentId': '', 'instanceParentResponsetype': '', 'instanceId': '', 'instanceParentExternalId': '', 'instanceParentEcmSequence': '', 'channel': '01392275379456409617', 'parent_channel': 'SHIKSHALOKAM', 'submissionNumber': 1, 'submissionTitle': 'Observation 1', 'criteriaLevelReport': 'False', 'isRubricDriven': False, 'userProfile': ''}
-
+slObservation = {
+  "roleTitle": "",
+  "userBoardName": "",
+  "userType": "",
+  "organisationId": "",
+  "organisationName": "",
+  "observationSubmissionId": "",
+  "appName": "",
+  "solutionType": "",
+  "entity": "",
+  "entityExternalId": "",
+  "course": "",
+  "createdBy": "",
+  "isAPrivateProgram": "",
+  "programExternalId": "",
+  "programId": "",
+  "programName": "",
+  "programDescription": "",
+  "solutionExternalId": "",
+  "solutionId": "",
+  "observationId": "",
+  "criteriaExternalId": "",
+  "criteriaName": "",
+  "criteriaDescription": "",
+  "section": "",
+  "solutionName": "",
+  "scoringSystem": "",
+  "solutionDescription": "",
+  "questionSequenceByEcm": "",
+  "entityType": "",
+  "observationName": "",
+  "questionId": "",
+  "questionAnswer": "",
+  "questionResponseType": "",
+  "questionResponseLabel": "",
+  "questionExternalId": "",
+  "questionName": "",
+  "questionECM": "",
+  "criteriaId": "",
+  "completedDate": "2024-03-26T16:14:10.690Z",
+  "createdAt": "2024-03-26T16:12:49.079Z",
+  "updatedAt": "2024-03-26T16:14:10.691Z",
+  "remarks": "",
+  "totalEvidences": "",
+  "instanceParentQuestion": "",
+  "instanceParentId": "",
+  "instanceParentResponsetype": "",
+  "instanceId": "",
+  "instanceParentExternalId": "",
+  "instanceParentEcmSequence": "",
+  "channel": "",
+  "parent_channel": "",
+  "submissionNumber": "",
+  "submissionTitle": "",
+  "criteriaLevelReport": "",
+  "isRubricDriven": "",
+  "userProfile": ""
+}
 slObservationMeta = {
     "completedDate": "2024-03-22T05:55:59.040Z",
-    "createdBy": "6778cc2b-8075-49a4-9e7d-deafa4d0d9ef",
-    "entity": "567",
+    "createdBy": "",
+    "entity": "",
     "entityExternalId": "",
-    "entityType": "course",
-    "isAPrivateProgram": "false",
-    "observationName": "dummy_test",
-    "observationSubmissionId": "65fd1d6fd2bc56000784c18d",
-    "observationId": "65e82048fa030d00078647df",
+    "entityType": "",
+    "isAPrivateProgram": "",
+    "observationName": "",
+    "observationSubmissionId": "",
+    "observationId": "",
     "organisationName": "",
-    "solutionType": "observation_with_out_rubric",
-    "solutionExternalId": "b048d95a-d490-11ee-a31f-a9298aa1a2bb-OBSERVATION-dummy",
-    "solutionId": "65dc65f9a9d408000795b4c8",
-    "solutionName": "dummy_test",
+    "solutionType": "",
+    "solutionExternalId": "",
+    "solutionId": "",
+    "solutionName": "",
     "userType": "",
     "userProfile": "",
     "createdAt": "2024-03-22T05:55:59.040Z"
@@ -38,79 +94,79 @@ slObservationMeta = {
 
 slObservationStatusStarted = {
     "startedAt": "2024-04-22T13:37:28.289Z",
-    "observationSubmissionId": "66264eabf805f30008243712"
+    "observationSubmissionId": ""
 }
 
 slObservationStatusInprogress = {
     "inprogressAt": "2024-04-22T13:37:28.289Z",
-    "observationSubmissionId": "66264eabf805f30008243712"
+    "observationSubmissionId": ""
 }
 
 slObservationStatusCompleted = {
     "completedAt": "2024-04-22T13:37:28.289Z",
-    "observationSubmissionId": "66264eabf805f30008243712"
+    "observationSubmissionId": ""
 }
 
 slSurvey = {
     "completedDate": "2024-07-18T06:58:19.373Z",
     "createdAt": "2024-07-18T06:57:17.862Z",
-    "createdBy": "de5373f4-d74b-4210-80c3-0d7ec283e300",
-    "criteriaExternalId": "8d796bba-0c34-11ef-953a-c0a5e8ff50d1-1715060533757-SF",
-    "criteriaId": "6639bf35b0610e0008d23728",
-    "criteriaName": "Survey and Feedback",
+    "createdBy": "",
+    "criteriaExternalId": "",
+    "criteriaId": "",
+    "criteriaName": "",
     "evidence_count": "",
     "evidences": "",
-    "isAPrivateProgram": "false",
-    "organisation_id": "0126796199493140480",
-    "organisation_name": "Staging Custodian Organization",
-    "questionAnswer": "gyh",
-    "questionECM": "SF",
-    "questionExternalId": "SUR_TEST_001_1715060530351-1715060533762",
-    "questionId": "6639bf35b0610e0008d23717",
-    "questionName": "Enter your First question",
-    "questionResponseLabel": "'gyh'",
-    "questionResponseLabel_number": 0,
-    "questionResponseType": "text",
+    "isAPrivateProgram": "",
+    "organisation_id": "",
+    "organisation_name": "",
+    "questionAnswer": "",
+    "questionECM": "",
+    "questionExternalId": "",
+    "questionId": "",
+    "questionName": "",
+    "questionResponseLabel": "",
+    "questionResponseLabel_number": "",
+    "questionResponseType": "",
     "remarks": "",
-    "solutionExternalId": "8d796bba-0c34-11ef-953a-c0a5e8ff50d1-1715060533757",
-    "solutionId": "6639bf35b0610e0008d2372c",
-    "solutionName": "testing survey for start date and end date",
-    "surveyId": "6698bccde504bc0008bc06c2",
-    "surveyName": "testing survey for start date and end date",
-    "surveySubmissionId": "6698bccde504bc0008bc06cb",
-    "total_evidences": "0",
+    "solutionExternalId": "",
+    "solutionId": "",
+    "solutionName": "",
+    "surveyId": "",
+    "surveyName": "",
+    "surveySubmissionId": "",
+    "total_evidences": "",
     "updatedAt": "2024-07-18T06:58:19.373Z",
-    "user_type": "administrator"
+    "user_type": ""
 }
 
 slSurveyMeta = {
     "completedDate": "2024-04-22T13:37:28.289Z",
-    "createdBy": "539de87f-8f72-479a-be03-a87cb4e2d7fc",
+    "createdBy": "",
     "createdAt": "2024-04-22T11:48:59.585Z",
-    "isAPrivateProgram": "false",
+    "isAPrivateProgram": "",
     "organisationName": "",
-    "solutionExternalId": "dd73c82e-008a-11ef-a7a3-ac12038f3f22-1713778193064",
-    "solutionId": "66262e11f805f300082438ff",
-    "solutionName": "dummy-event",
-    "surveyName": "dummy-event",
-    "surveySubmissionId": "66264eabf805f30008243712",
-    "surveyId": "66264eabf805f3000824396f",
+    "solutionExternalId": "",
+    "solutionId": "",
+    "solutionName": "",
+    "surveyName": "",
+    "surveySubmissionId": "",
+    "surveyId": "",
     "userProfile": ""
 }
 
 slSurveyStatusStarted = {
     "startedAt": "2024-04-22T13:37:28.289Z",
-    "surveySubmissionId": "66264eabf805f30008243712"
+    "surveySubmissionId": ""
 }
 
 slSurveyStatusInprogress = {
     "inprogressAt": "2024-04-22T13:37:28.289Z",
-    "surveySubmissionId": "66264eabf805f30008243712"
+    "surveySubmissionId": ""
 }
 
 slSurveyStatusCompleted = {
     "completedAt": "2024-04-22T13:37:28.289Z",
-    "surveySubmissionId": "66264eabf805f30008243712"
+    "surveySubmissionId": ""
 }
 
 def send_to_kafka(topic, data):
